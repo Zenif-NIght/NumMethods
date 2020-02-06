3e8d56ea86ad39d5f6d2a561f1385cf6fc7602cc
Christopher <Christophertaylorallred@gmail.com>
2020-02-05 21:29:13 -0700

    Started

---
+++ HW/HW3/NM_HW3.py
@@ -0,0 +1,50 @@
+'''
+Homework 3 is dueonlinethrough Canvas in PDF format by 11:59PM on Friday February 7.You are required to submit
+ code for all functions and/or subroutines built to solvethese problems, which is designed to be easy to read 
+ and understand, in your chosenprogramming language,and which you have written yourself.  The text fromyour 
+ code should both be copied into a single PDF file submitted on canvas.Yoursubmitted PDF must also include 
+ responses to any assigned questions,which for problems requiring programming should be based on outputfrom 
+ your code.   For  example,  if  you  are  asked  to  find  numerical  answers  to  aproblem, the numbers
+  should be included in your submission.
+  
+  1.  Given the equations
+  10*x1+ 2*x2−x3= 27
+  −3*x1−6*x2+ 2*x3=−61.5
+  x1+x2+ 5x3=−21.5
+
+  [ 10 2 -1 |27 
+    -3]
+
+  (a)  Solve using naive Gauss elimination (by hand).  Show all steps of the com-putation.
+  
+  (b)  Substitute your results into the original equations to check your answers.
+  
+
+  2.  Given the equations
+  x1+ 2*x2−*x3= 2
+  5*x1+ 2*x2+ 2*x3= 9
+  −3*x1+ 5*x2−*x3= 1,
+  (a)  Solve by Gauss elimination with partial pivoting using code you have writtenyourself
+  (see Figure 9.6 on page 268 of text for pseudocode - beware of typosand/or unneccessary components!).
+
+  (b)  Substitute your results into the original equations to check your answers.
+
+
+  3.  Given the equations
+
+  8*x1+ 4*x2−*x3= 11
+  −2*x1+ 5*x2+*x3= 4
+  2*x1−*x2+ 6*x3= 7
+
+  (a)  Solve using LU decomposition without pivoting (by hand).  Show all steps ofthe computation.
+  (b)  Determine the matrix inverse using LU decomposition (by hand), and verifythat [A][A]−1= [I].4.  
+  Given the equations
+  2*x1−6*x2−x3=−38
+  −3*x1−*x2+ 7*x3=−34
+  −8*x1+x2−2*x3=−20,
+  
+  (a)  Solve  using  LU  decomposition  with  partial  pivoting  using  code  you  havewritten  yourself 
+   (see  Figure  10.2  on  page  286  for  pseudocode  -  beware  oftypos and/or unnecessary components!).
+   
+   (b)  Determine the matrix inverse using code you have written yourself (see Fig-ure 10.5 on page 290 for 
+   pseudocode - beware of typos and/or unnecessarycomponents!).
