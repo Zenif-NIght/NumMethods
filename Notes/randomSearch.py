ae00a352cb9b081a212622bb3667ce5f07c3ad7a
Christopher <Christophertaylorallred@gmail.com>
2020-02-11 13:43:45 -0700

    Pushing things around

---
+++ Notes/randomSearch.py
@@ -0,0 +1,10 @@
+import random
+def randomSearch(n):
+    fmax = 
+    Xopt = a
+    Yopt = c
+    random.seed(10)
+    for j in range(0,n-1):
+        x = a+ random.random()*(b-a)
+        x = c+ random.random()*(d-c)
+        fj = f(x,y)
